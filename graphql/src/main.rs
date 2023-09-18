ba9180fd515dc32b1d4b944dcf432d0311b7194c
YassinEldeeb <yassineldeeb94@gmail.com>
2023-09-18 12:18:19 -0700

    setup juniper graphql

--- graphql/src/main.rs
+++ graphql/src/main.rs
@@ -1,11 +1,103 @@
+use std::collections::HashMap;
+
 use actix_web::{
     middleware,
-    web::{self},
-    App, HttpServer,
+    web::{self, Data},
+    App, Error, HttpResponse, HttpServer,
 };
+use juniper::{graphql_object, EmptyMutation, EmptySubscription, GraphQLObject, RootNode};
+use juniper_actix::{graphiql_handler, graphql_handler, playground_handler};
 
-async fn handle_incoming_req() -> String {
-    String::from("I can handle requests!")
+#[derive(Clone, GraphQLObject)]
+pub struct User {
+    id: i32,
+    name: String,
+}
+
+#[derive(Clone, Default)]
+pub struct Database {
+    ///this could be a database connection
+    users: HashMap<i32, User>,
+}
+impl Database {
+    pub fn new() -> Database {
+        let mut users = HashMap::new();
+        users.insert(
+            1,
+            User {
+                id: 1,
+                name: "Aron".into(),
+            },
+        );
+        users.insert(
+            2,
+            User {
+                id: 2,
+                name: "Bea".into(),
+            },
+        );
+        users.insert(
+            3,
+            User {
+                id: 3,
+                name: "Carl".into(),
+            },
+        );
+        users.insert(
+            4,
+            User {
+                id: 4,
+                name: "Dora".into(),
+            },
+        );
+        Database { users }
+    }
+    pub fn get_user(&self, id: &i32) -> Option<&User> {
+        self.users.get(id)
+    }
+}
+
+// To make our Database usable by Juniper, we have to implement a marker trait.
+impl juniper::Context for Database {}
+
+struct Query;
+#[graphql_object(context = Database)]
+impl Query {
+    fn api_version() -> &'static str {
+        "1.0"
+    }
+
+    fn user(
+        context: &Database,
+        #[graphql(description = "id of the user")] id: i32,
+    ) -> Option<&User> {
+        context.get_user(&id)
+    }
+}
+
+type Schema = RootNode<'static, Query, EmptyMutation<Database>, EmptySubscription<Database>>;
+
+fn schema() -> Schema {
+    Schema::new(
+        Query,
+        EmptyMutation::<Database>::new(),
+        EmptySubscription::<Database>::new(),
+    )
+}
+
+async fn graphiql_route() -> Result<HttpResponse, Error> {
+    graphiql_handler("/graphql", None).await
+}
+async fn playground_route() -> Result<HttpResponse, Error> {
+    playground_handler("/graphql", None).await
+}
+async fn graphql_route(
+    req: actix_web::HttpRequest,
+    payload: actix_web::web::Payload,
+    schema: web::Data<Schema>,
+) -> Result<HttpResponse, Error> {
+    let context = Database::new();
+    graphql_handler(&schema, &context, req, payload).await
 }
 
 #[actix_web::main]
@@ -13,10 +105,16 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
 
     let server = HttpServer::new(move || {
-        App::new().wrap(middleware::Logger::default()).service(
-            web::resource("/").route(web::get().to(handle_incoming_req)), // .route(web::get().to(graphql_route)),
-        )
+        App::new()
+            .app_data(Data::new(schema()))
+            .wrap(middleware::Logger::default())
+            .service(
+                web::resource("/graphql")
+                    .route(web::get().to(playground_route))
+                    .route(web::post().to(graphql_route)),
+            )
+            .service(web::resource("/graphiql").route(web::get().to(graphiql_route)))
     });
-    server.bind("127.0.0.1:8080").unwrap().run().await
+    server.bind("127.0.0.1:4000").unwrap().run().await
 }
-// now go to http://127.0.0.1:8080/
+// now go to http://127.0.0.1:4000/
