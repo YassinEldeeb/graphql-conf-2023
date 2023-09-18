742a54ce4e35280c0bd46a676228ea5319b75e37
YassinEldeeb <yassineldeeb94@gmail.com>
2023-09-18 10:42:51 -0700

    setup actix web http server

--- graphql/src/main.rs
+++ graphql/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{
+    middleware,
+    web::{self},
+    App, HttpServer,
+};
+
+async fn handle_incoming_req() -> String {
+    String::from("I can handle requests!")
 }
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init();
+
+    let server = HttpServer::new(move || {
+        App::new().wrap(middleware::Logger::default()).service(
+            web::resource("/").route(web::get().to(handle_incoming_req)), // .route(web::get().to(graphql_route)),
+        )
+    });
+    server.bind("127.0.0.1:8080").unwrap().run().await
+}
+// now go to http://127.0.0.1:8080/
